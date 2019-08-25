d82f3d238ca105e07b4bd19428a5b11ba24b7fab
escleur <escleur@gmail.com>
2019-08-24 22:42:46 -0300

    ejercicio clase 2

--- clase2-1/src/clase2-1.c
+++ clase2-1/src/clase2-1.c
@@ -11,7 +11,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int calculaMaximoMinimoPromedio(int *maximo, int *minimo, int *promedio, int *cantidad);
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int maximo;
+	int minimo;
+	int promedio;
+	int cantidad;
+
+	if(calculaMaximoMinimoPromedio(&maximo, &minimo, &promedio, &cantidad) == 0){
+		printf("El maximo es %d el minimo es %d el promedio es %d y la cantidad de casos fue %d", maximo, minimo, promedio, cantidad);
+	}
 	return EXIT_SUCCESS;
+}
+
+int calculaMaximoMinimoPromedio(int *maximo, int *minimo, int *promedio, int *cantidad){
+	int retorno = -1;
+	char pregunta;
+	int flag = 0;
+	int numero;
+
+	*cantidad = 0;
+	*promedio = 0;
+
+	do{
+		printf("Ingrese un numero\n");
+		scanf("%d", &numero);
+
+		(*cantidad)++;
+		(*promedio) += numero;
+
+		if(flag == 0){
+			flag = 1;
+			*maximo = numero;
+			*minimo = numero;
+		}else if(numero > *maximo){
+			*maximo = numero;
+		}else if(numero < *minimo){
+			*minimo = numero;
+		}
+
+
+
+		printf("Ingrese s para continuar\n");
+		scanf("%s", &pregunta);
+	}while(pregunta == 's');
+
+	(*promedio) /= (*cantidad);
+	retorno = 0;
+
+	return retorno;
 }
