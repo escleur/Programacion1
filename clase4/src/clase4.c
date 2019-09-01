e2c8e21dbbe2cead28743d82fab688cb7dc75f78
gabriel <gabriel@gabriel-System-Product-Name>
2019-08-31 22:20:48 -0300

    Clase 4

---
+++ clase4/src/clase4.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : clase4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+
+#define EXIT_ERROR -1
+
+int main(void)
+{
+
+	char resultado[60];
+	if(getString(&resultado,"Ingrese su nombre?\n","Error\n",2,50,2)==0)
+	{
+		printf("El resultado es: %s",resultado);
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ clase4/src/utn.c
@@ -0,0 +1,156 @@
+/*
+ * utn.c
+ *
+ *  Created on: 31 ago. 2019
+ *      Author: gas
+ */
+
+
+#include <stdio.h>
+//#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utn.h"
+
+//Linux
+//#define PURGAR __fpurge(stdin);
+//Windows
+#define PURGAR fflush(stdin);
+
+
+int getInt(	int *resultado,
+			char *mensaje,
+			char *mensajeError,
+			int minimo,
+			int maximo,
+			int reintentos)
+{
+	int retorno = EXIT_ERROR;
+	int buffer;
+	if(	resultado != NULL &&
+		mensaje	!= NULL &&
+		mensajeError != NULL &&
+		minimo < maximo &&
+		reintentos >= 0)
+	{
+		do
+		{
+			printf("%s",mensaje);
+			PURGAR
+			if(scanf("%d",&buffer)==1)
+			{
+				if(buffer >= minimo && buffer <= maximo)
+				{
+					retorno = EXIT_SUCCESS;
+					*resultado = buffer;
+					break;
+				}
+			}
+			printf("%s",mensajeError);
+			reintentos--;
+		}while(reintentos >= 0);
+	}
+	return retorno;
+}
+int getFloat(float *resultado,
+			 char *mensaje,
+			 char *mensajeError,
+			 float minimo,
+			 float maximo,
+			 int reintentos)
+{
+	int retorno = EXIT_ERROR;
+	float buffer;
+	if(	resultado != NULL &&
+		mensaje	!= NULL &&
+		mensajeError != NULL &&
+		minimo < maximo &&
+		reintentos >= 0)
+	{
+		do
+		{
+			printf("%s",mensaje);
+			PURGAR
+			if(scanf("%f",&buffer)==1)
+			{
+				if(buffer >= minimo && buffer <= maximo)
+				{
+					retorno = EXIT_SUCCESS;
+					*resultado = buffer;
+					break;
+				}
+			}
+			printf("%s",mensajeError);
+			reintentos--;
+		}while(reintentos >= 0);
+	}
+	return retorno;
+}
+int getChar( char *resultado,
+			  char *mensaje,
+			  char *mensajeError,
+			  char minimo,
+			  char maximo,
+			  int reintentos)
+{
+	int retorno = EXIT_ERROR;
+	char buffer;
+	if(	resultado != NULL &&
+		mensaje	!= NULL &&
+		mensajeError != NULL &&
+		minimo < maximo &&
+		reintentos >= 0)
+	{
+		do
+		{
+			printf("%s",mensaje);
+			PURGAR
+			if(scanf("%c",&buffer)==1)
+			{
+				if(buffer >= minimo && buffer <= maximo)
+				{
+					retorno = EXIT_SUCCESS;
+					*resultado = buffer;
+					break;
+				}
+			}
+			printf("%s",mensajeError);
+			reintentos--;
+		}while(reintentos >= 0);
+	}
+	return retorno;
+}
+int getString (char *resultado,
+		      char *mensaje,
+			  char *mensajeError,
+			  int minimo,
+			  int maximo,
+			  int reintentos)
+{
+	int retorno = EXIT_ERROR;
+	char buffer[500];
+	if(	resultado != NULL &&
+		mensaje	!= NULL &&
+		mensajeError != NULL &&
+		minimo < maximo &&
+		reintentos >= 0)
+	{
+		do
+		{
+			printf("%s",mensaje);
+			PURGAR
+			if(scanf("%s",buffer)==1)
+			{
+				if(strlen(buffer) >= minimo && strlen(buffer) <= maximo)
+				{
+					retorno = EXIT_SUCCESS;
+					strcpy(resultado, buffer);
+					break;
+				}
+			}
+			printf("%s",mensajeError);
+			reintentos--;
+		}while(reintentos >= 0);
+	}
+	return retorno;
+}
