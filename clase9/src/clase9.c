07ef8bccdeeb8247e3f67477e162b5af3eea1dd2
alumno <sanglahector@gmail.com>
2019-09-17 22:20:16 -0300

    abm

---
+++ clase9/src/clase9.c
@@ -0,0 +1,44 @@
+/*
+ ============================================================================
+ Name        : Clase_08.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utn.h"
+#include "Empleado.h"
+
+
+int main(void)
+{
+
+	struct sEmpleado aEmpleados[1000];
+	struct sEmpleado bEmpleado;
+	int i;
+	int idEmpleado = 0;
+
+	for(i=0;i<3;i++){
+
+		getString(bEmpleado.nombre,"Ingrese el nombre",
+				"ERROR", 1, 49, 2);
+
+		getString(bEmpleado.apellido,"Ingrese el apellido",
+						"ERROR", 1, 49, 2);
+
+		bEmpleado.idEmpleado = idEmpleado;
+		idEmpleado++;
+		bEmpleado.status = STATUS_NOT_EMPTY;
+
+		aEmpleados[i] = bEmpleado;
+	}
+	imprimirArrayEmpleados(aEmpleados,3);
+
+	return EXIT_SUCCESS;
+}
